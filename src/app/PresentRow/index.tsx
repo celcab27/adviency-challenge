1d3980d3c51446a984f85a6eee8519875cfbe6a7
celcab27 <cabreraceleste672@gmail.com>
2024-01-11 00:45:47 -0300

    Added the posibility of adding a quantity and an image. Now values are stored in localStorage

--- src/app/PresentRow/index.tsx
+++ src/app/PresentRow/index.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 import deleteIcon from "../../assets/icons/deleteIcon.svg";
+import { Present } from "../page";
 
 interface PresentRowProps {
-  item: string;
+  item: Present;
   onClick: (value: string) => void;
 }
 
@@ -14,10 +15,23 @@ export default function PresentRow({ item, onClick }: PresentRowProps) {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
+        marginBottom: 16,
       }}
     >
-      <p className="text-black">{item}</p>;
-      <div onClick={() => onClick(item)}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src={item.image}
+          alt="present image"
+          width={24}
+          height={24}
+          style={{ width: 36, height: 36, borderRadius: 4, marginRight: 16 }}
+        />
+        <p className="text-black">{item.name}</p>;
+        {item.quantity > 1 && (
+          <p className="text-black">{`x${item.quantity}`}</p>
+        )}
+      </div>
+      <div onClick={() => onClick(item.name)}>
         <Image
           alt="delete icon"
           src={deleteIcon}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PresentRow from "./PresentRow";
 
+export interface Present {
+  name: string;
+  quantity: number;
+  image: string;
+}
+
 export default function Home() {
-  const [presents, setPresents] = useState([
-    "Regalo 1",
-    "Regalo 2",
-    "Regalo 3",
-  ]);
+  const defaultPresents = localStorage.getItem("presents");
+  const [presents, setPresents] = useState<Present[]>(
+    defaultPresents?.length ? JSON.parse(defaultPresents) : []
+  );
   const [newValue, setNewValue] = useState("");
+  const [newQuantity, setNewQuantity] = useState(1);
+  const [newImage, setNewImage] = useState("");
 
   const handleClick = (value: string) => {
-    setPresents((prevState) => prevState.filter((item) => item !== value));
+    setPresents((prevState) => prevState.filter((item) => item.name !== value));
   };
 
+  useEffect(() => {
+    localStorage.setItem("presents", JSON.stringify(presents));
+  }, [presents]);
+
+  console.log("presents", presents);
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-center p-24 bg-white bg-[url('https://www.itakeyou.co.uk/wp-content/uploads/2022/10/christmas-wallpapers.jpg')] bg-no-repeat"
@@ -21,7 +33,7 @@ export default function Home() {
     >
       <div
         className="border-black h-3/4 bg-white rounded-lg shadow-2xl"
-        style={{ height: 600, width: 400 }}
+        style={{ height: 600, width: 700 }}
       >
         <div>
           <p
@@ -40,17 +52,49 @@ export default function Home() {
               marginBottom: 24,
             }}
           >
-            <input
-              placeholder="Regalo 1..."
-              onChange={(e) => setNewValue(e.target.value)}
-              style={{
-                borderWidth: 0.5,
-                borderColor: "#808080",
-                borderRadius: 4,
-                padding: "4px 8px",
-                color: "black",
-              }}
-            />
+            <div>
+              <input
+                placeholder="Regalo 1..."
+                onChange={(e) => setNewValue(e.target.value)}
+                style={{
+                  borderWidth: 0.5,
+                  borderColor: "#808080",
+                  borderRadius: 4,
+                  padding: "4px 8px",
+                  color: "black",
+                  width: 200,
+                }}
+              />
+              <input
+                placeholder="http://image..."
+                onChange={(e) => setNewImage(e.target.value)}
+                style={{
+                  borderWidth: 0.5,
+                  borderColor: "#808080",
+                  borderRadius: 4,
+                  padding: "4px 8px",
+                  color: "black",
+                  width: 200,
+                  marginLeft: 8,
+                }}
+              />
+              <input
+                type="number"
+                onChange={(e) => setNewQuantity(Number(e.target.value))}
+                defaultValue={1}
+                value={newQuantity}
+                style={{
+                  width: 50,
+                  marginLeft: 8,
+                  marginRight: 16,
+                  border: "0.5px solid rgb(128, 128, 128)",
+                  borderRadius: 4,
+                  height: 32,
+                  color: "black",
+                  paddingLeft: 4,
+                }}
+              />
+            </div>
             <button
               className="text-wite"
               style={{
@@ -59,8 +103,30 @@ export default function Home() {
                 borderRadius: 8,
               }}
               onClick={() => {
-                if (newValue && presents.indexOf(newValue) === -1) {
-                  setPresents((prev) => [...prev, newValue]);
+                if (newValue) {
+                  if (presents.some((item) => item.name === newValue)) {
+                    setPresents((prevState) => {
+                      // Crear un nuevo array con las actualizaciones
+                      return prevState.map((item) =>
+                        item.name === newValue
+                          ? {
+                              ...item,
+                              quantity: item.quantity + newQuantity,
+                              image: newImage,
+                            }
+                          : item
+                      );
+                    });
+                  } else {
+                    setPresents((prev) => [
+                      ...prev,
+                      {
+                        name: newValue,
+                        quantity: newQuantity,
+                        image: newImage,
+                      },
+                    ]);
+                  }
                 }
               }}
             >
